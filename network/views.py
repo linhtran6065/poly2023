c1459939851ffadab92b295672e5b0bb90b20d1d
Nguyen Duc Thanh <nguyenducthanh240903@gmail.com>
2023-06-15 23:25:32 +0700

    add quizz

--- network/views.py
+++ network/views.py
@@ -96,6 +96,70 @@ def register(request):
     else:
         return render(request, "network/register.html")
 
+def quizz(request):
+    radioQuestions = [
+        {
+            "number": 1,
+            "question": "I have a kind word for everyone",
+            "options": ["1", "2", "3"],
+        },
+        {
+            "number": 2,
+            "question": "I am always prepared",
+            "options": ["1", "2", "3"],
+        },
+        {
+            "number": 3,
+            "question": "I feel comfortable around people",
+            "options": ["1", "2", "3"],
+        },
+        {
+            "number": 4,
+            "question": "I often feel blue",
+            "options": ["1", "2", "3"],
+        },
+        {
+            "number": 5,
+            "question": "I am very good at identifying the emotions I am feeling",
+            "options": ["1", "2", "3"],
+        },
+        {
+            "number": 6,
+            "question": "I believe in the importance of art",
+            "options": ["1", "2", "3"],
+        },
+        {
+            "number": 7,
+            "question": "I am the life of the party",
+            "options": ["1", "2", "3"],
+        },
+        {
+            "number": 8,
+            "question": "I am very good at reading body language",
+            "options": ["1", "2", "3"],
+        },
+        {
+            "number": 9,
+            "question": "There are many things that I do not like about myself",
+            "options": ["1", "2", "3"],
+        },
+        {
+            "number": 10,
+            "question": "My moods change easily",
+            "options": ["1", "2", "3"],
+        }
+    ]
+
+
+    if request.method == "GET":
+
+        return render(request, "network/quizz.html", {
+            "radioQuestions": radioQuestions,
+        })
+
+    if request.method == "POST":
+        # print(request.POST)
+        return JsonResponse({"message": "POST request received.", "data": request.POST}, status=200)
 
 
 def profile(request, username):
